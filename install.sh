2a7d12e55f05d779c73a888b6f6f36fbe2184c76
Sk parveZ <alm04190@gmail.com>
2026-04-02 23:28:13 +0600

    Update install script to include tool download and setup

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 clear
+echo "⚙️ Downloading Tool Files..."
+cd $HOME
+rm -rf ParveZ-downloader
+git clone https://github.com/alm04190-gif/ParveZ-downloader.git
+cd ParveZ-downloader
+
 echo "⚙️ Installing Requirements..."
 pip install -r requirements.txt
 
 echo "⚙️ Setting up shortcut..."
 echo -e '#!/bin/bash\ncd $HOME/ParveZ-downloader && python main.py' > $PREFIX/bin/download
-chmod +x $PREFIX/bin/download
+chmod +x $PREFIX/bin/report
 
 clear
 echo "========================================"
